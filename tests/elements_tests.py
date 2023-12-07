929d0adcfd7906652c0925de93eb2282ceb7f5b4
Дмитрий <119815063+111denver111@users.noreply.github.com>
2023-12-07 11:38:38 +0300

    new test checkbox

--- tests/elements_tests.py
+++ tests/elements_tests.py
@@ -1,10 +1,11 @@
-from pages.elements_page import TextBoxPage
+import time
+
+from pages.elements_page import TextBoxPage, CheckBoxPage
 from conftest import driver
 
 
 class TestElements:
     class TestTextBox:
-
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
             text_box_page.open()
@@ -14,4 +15,16 @@ class TestElements:
             assert email == output_email, "the email does not match"
             assert current_address == output_cur_addr, "the current_address does not match"
             assert permanent_address == output_per_addr, "the permanent_address does not match"
-            # Тест выливки 1
+
+    class TestsCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            time.sleep(5)
+            print(input_checkbox)
+            print(output_result)
+            assert input_checkbox == output_result, "checkboxes have not been selected"
